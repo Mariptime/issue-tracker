b5f4f5ab6e4cce4da250148b22480d1ce2600c55
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2024-05-20 15:31:30 -0500

    refactoring the POST API

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,21 +1,26 @@
-import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod'
 import prisma from "@/prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
 
 const createIssueScema = z.object({
     title: z.string().min(1).max(255),
-    description: z.string().min(1),
+    description: z.string().min(1)
 });
 
 export async function POST(request: NextRequest) {
     const body = await request.json();
     const validation = createIssueScema.safeParse(body);
+
     if (!validation.success) {
-        return NextResponse.json({ error: validation.error.errors }, { status: 400 });
+        return NextResponse.json(validation.error.errors, { status: 400 });
     }
 
     const newIssue = await prisma.issue.create({
-        data: { title: body.title, description: body.description }
+        data:
+        {
+            title: body.title,
+            description: body.description
+        }
     });
 
     return NextResponse.json(newIssue, { status: 201 });
