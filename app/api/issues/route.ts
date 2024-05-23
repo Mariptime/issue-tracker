fd3339d258044dc7e96310ab64281d07853267b0
Akshay Nagarajan <akshay.h.nagarajan@gmail.com>
2024-05-23 18:46:59 -0500

    Handling Form Errors

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -3,8 +3,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Must include a Title (String)').max(255),
+    description: z.string().min(1, 'Must include a Description (String)')
 });
 
 export async function POST(request: NextRequest) {
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
 
     if (!validation.success) {
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format(), { status: 400 });
     }
 
     const newIssue = await prisma.issue.create({
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,11 +1,18 @@
 'use client';
-import { Button, TextField } from '@radix-ui/themes';
-import SimpleMDE from "react-simplemde-editor";
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import dynamic from 'next/dynamic';
 import { useForm, Controller } from 'react-hook-form';
 import axios from 'axios';
 import "easymde/dist/easymde.min.css";
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { FaInfoCircle } from 'react-icons/fa';
 
+//quick fix for navigator error
+const SimpleMDE = dynamic(
+    () => import('react-simplemde-editor'),
+    { ssr: false } // This will load the component only on client side
+);
 interface IssueForm {
     title: string;
     description: string;
@@ -14,19 +21,34 @@ interface IssueForm {
 const newIssuePage = () => {
     const router = useRouter();
     const { register, control, handleSubmit } = useForm<IssueForm>();
+    const [error, setError] = useState('');
+
     return (
-        <form className='max-w-xl space-y-2' onSubmit={handleSubmit(async (data) => {
-            await axios.post('/api/issues', data);
-            router.push('/issues');
-        })}>
-            <TextField.Root placeholder='Title' {...register('title')} />
-            <Controller
-                name='description'
-                control={control}
-                render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
-            />
-            <Button>Submit New Issue</Button>
-        </form>
+        <div className='max-w-xl items-center'>
+            {error && (<Callout.Root color='red' className='mb-5'>
+                <Callout.Icon>
+                    <FaInfoCircle />
+                </Callout.Icon>
+                <Callout.Text> {error} </Callout.Text>
+            </Callout.Root>)}
+
+            <form className='space-y-2' onSubmit={handleSubmit(async (data) => {
+                try {
+                    await axios.post('/api/issues', data);
+                    router.push('/issues');
+                } catch (error) {
+                    setError('Unknown Error Occured');
+                }
+            })}>
+                <TextField.Root placeholder='Title' {...register('title')} />
+                <Controller
+                    name='description'
+                    control={control}
+                    render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
+                />
+                <Button>Submit New Issue</Button>
+            </form>
+        </div>
     )
 }
 
